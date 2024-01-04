5ae81badd48d174bc31b3dcce71f3c1592e30af0
Yaw Boateng <yboateng@smu.edu>
2024-01-04 11:17:54 -0600

    more packages and debgging

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-import streamlit as st
-from dotenv import load_dotenv
-from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
-from langchain.llms import HuggingFaceHub
+import streamlit as streamlit
+
+def main():
+    print("hello world")
+
+if __name__ == '__main__':
+    main()
